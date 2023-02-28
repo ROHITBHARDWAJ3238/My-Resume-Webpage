af11fd80a7fb83955ac2c2ddd0fb4ee76d711303
ROHITBHARDWAJ3238 <87866699+ROHITBHARDWAJ3238@users.noreply.github.com>
2023-02-28 21:56:17 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/*var targetPos = 1500;
+var currentPos = 0;
+
+var scrollInterval = 
+setInterval(function(){
+    if (currentPos >= targetPos ){
+        clearInterval(scrollInterval)
+        return;
+    }
+    currentPos += 50;
+    window.scrollBy(0,20);
+} , 50); */
